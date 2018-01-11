138a716a753ef5923335993fcc63729c71921de6
Guest User <Guest@Epicodus-7D.local>
2018-01-11 10:40:20 -0800

    add styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
   var age = prompt("Enter your current age");
   if (age > 20) {
     $(".under21").hide();
     $(".over21").show();
   }
 });
+*/
