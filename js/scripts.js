775467d0c0c9aba3f7bfe974b984e79ef5d13a5a
Guest User <Guest@Epicodus-7D.local>
2018-01-11 16:46:33 -0800

    create a way to mark columns with jquery

--- js/scripts.js
+++ js/scripts.js
@@ -3,36 +3,30 @@ $( function() {
  } );
 
 $(document).ready(function() {
-  $( ".container .row+.row+.row .col" ).first().click(function() {
-    alert("test");
-  });
+
   var elements = $(".container .row");
 
-  //var elements2 = $(".container .row:nth-child(1) .col");
-
-
-//so we can do per row now we just need to acces the specific row
 
   var k = 1;
 
   for (var i = 0; i < elements.length; i++) {
     console.log("i"+i);
-    //add classes to the rows so we can target them
     $(".container .row:nth-child("+k+")").addClass("row"+i);
+    var columnNumber = 0;
 
-    var itemsInside = $(".row"+i).text();
-    console.log(itemsInside);
-
-    //I have to figure out a way to create the second array - perhaps based
-
+    $(".container .row"+i).children(".col").each(function () {
+      $(this).attr('id', i+"-column"+columnNumber);
+      columnNumber++;
+    });
     k++;
   }
 
+  var action = ["Saab", "Volvo", "BMW"];
+
+  $(".container .row .col").click(function() {
+    alert(event.target.id);
+  });
+
+
+
 });
-
-
-/*
-  For each row
-    for each column in row
-
-*/
